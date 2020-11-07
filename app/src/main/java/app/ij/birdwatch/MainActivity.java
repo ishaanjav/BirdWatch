bb0c1eaf8f5d6e79b0e93fe0e92cbd3190c89d06
Madhu2244 <madhuxsharma@gmail.com>
2020-11-07 11:20:14 -0600

    Added Camera and Gallery Compatibility

--- app/src/main/java/app/ij/birdwatch/MainActivity.java
+++ app/src/main/java/app/ij/birdwatch/MainActivity.java
@@ -1,19 +1,53 @@
 package app.ij.birdwatch;
 
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity
+{
+    int PICTURE_RESULT = 1, GALLERY_RESULT = 2;
+    Button camera, gallery;
+    ContentValues values;
+    Uri imageUri;
+    Bitmap img;
+    String imageurl;
+    ImageView imageView;
+    int REQUEST_CAMERA = 1034, REQUEST_GALLERY = 1000, REQUEST_GALLERY_FROM_CAMERA = 234;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +64,202 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    void buttons() {
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ResultsActivity.class);
+                Log.wtf("*Passing URI", "Is null: " + (imageUri == null));
+                intent.putExtra("image", imageUri);
+                startActivity(intent);
+            }
+        });
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Check if we have permission
+                if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA);
+                } else {
+                    launchCamera();
+                }
+            }
+        });
+        gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                        == PackageManager.PERMISSION_DENIED) {
+                    //permission not granted request it.
+                    String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+                    //show popupƒ
+                    requestPermissions(permissions, REQUEST_GALLERY);
+                } else {
+                    /* permission already granted */
+                    pickImageFromGallery();
+                }
+            }
+        });
+    }
+
+    private void pickImageFromGallery() {
+        //intent to pick image
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, GALLERY_RESULT);
+
+    }
+
+    // Get the result from the Camera/Gallery
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // They didn't cancel
+        if (resultCode == RESULT_OK) {
+            if (requestCode == PICTURE_RESULT) {
+                try {
+                    img = MediaStore.Images.Media.getBitmap(
+                            getContentResolver(), imageUri);
+                    img = handleSamplingAndRotationBitmap(getApplicationContext(), imageUri);
+                    img = centerCrop(img);
+                    imageView.setImageBitmap(img);
+                    imageurl = getRealPathFromURI(imageUri);
+                    makeToast("Got the image");
+                    next.setVisibility(View.VISIBLE);
+                    text.setVisibility(View.INVISIBLE);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else {
+                imageUri = data.getData();
+                try {
+                    img = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                    img = handleSamplingAndRotationBitmap(getApplicationContext(), imageUri);
+                    img = centerCrop(img);
+                    next.setVisibility(View.VISIBLE);
+                    text.setVisibility(View.INVISIBLE);
+                    imageView.setImageBitmap(img);
+                    makeToast("Got the image");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    makeToast("Error getting image");
+                    Log.wtf("*Gallery error: ", e.toString());
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CAMERA) {
+            if (checkSelfPermission(Manifest.permission.CAMERA)
+                    == PackageManager.PERMISSION_GRANTED) {
+                launchCamera();
+            }
+        } else if (requestCode == REQUEST_GALLERY) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                pickImageFromGallery();
+            }
+        } else if (requestCode == REQUEST_GALLERY_FROM_CAMERA) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                launchCamera();
+            }
+        }
+
+    }
+
+    void launchCamera() {
+        try {
+            values = new ContentValues();
+            imageUri = getContentResolver().insert(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+            startActivityForResult(intent, PICTURE_RESULT);
+        } catch (Exception e) {
+            Log.wtf("*Error launching Camera", e.toString());
+            if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_GALLERY_FROM_CAMERA);
+            } else {
+                pickImageFromGallery();
+            }
+        }
+    }
+
+    public Uri getImageUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+        return Uri.parse(path);
+    }
+
+    public static Bitmap centerCrop(Bitmap srcBmp) {
+        if (srcBmp.getWidth() >= srcBmp.getHeight()) {
+            Log.wtf("*Cropping", "Image width > height");
+            return Bitmap.createBitmap(srcBmp, srcBmp.getWidth() / 2 - srcBmp.getHeight() / 2, 0, srcBmp.getHeight(), srcBmp.getHeight());
+        }
+        return Bitmap.createBitmap(srcBmp, 0, srcBmp.getHeight() / 2 - srcBmp.getWidth() / 2, srcBmp.getWidth(), srcBmp.getWidth());
+    }
+
+
+    private static Bitmap rotateImage(Bitmap img, int degree) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(degree);
+        Bitmap rotatedImg = Bitmap.createBitmap(img, 0, 0, img.getWidth(), img.getHeight(), matrix, true);
+        img.recycle();
+        return rotatedImg;
+    }
+
+    private static Bitmap rotateImageIfRequired(Context context, Bitmap img, Uri selectedImage) throws IOException {
+        InputStream input = context.getContentResolver().openInputStream(selectedImage);
+        ExifInterface ei;
+        if (Build.VERSION.SDK_INT > 23)
+            ei = new ExifInterface(input);
+        else
+            ei = new ExifInterface(selectedImage.getPath());
+
+        int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+
+        switch (orientation) {
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                return rotateImage(img, 90);
+            case ExifInterface.ORIENTATION_ROTATE_180:
+                return rotateImage(img, 180);
+            case ExifInterface.ORIENTATION_ROTATE_270:
+                return rotateImage(img, 270);
+            default:
+                return img;
+        }
+    }
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    public String getRealPathFromURI(Uri contentUri) {
+        String[] proj = {MediaStore.Images.Media.DATA};
+        Cursor cursor = managedQuery(contentUri, proj, null, null, null);
+        int column_index = cursor
+                .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(column_index);
+    }
+
+    Toast t;
+
+    public void makeToast(String s) {
+        if (t != null) t.cancel();
+        t = Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG);
+        t.show();
     }
 }
