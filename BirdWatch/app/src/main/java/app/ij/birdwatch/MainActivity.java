3185bffda50fb87a05770ec5c77fe369e08acbb7
ishaanjav <ishaanjav@gmail.com>
2020-11-07 11:42:38 -0600

    Image from Camera

--- BirdWatch/app/src/main/java/app/ij/birdwatch/MainActivity.java
+++ BirdWatch/app/src/main/java/app/ij/birdwatch/MainActivity.java
@@ -1,35 +1,110 @@
 package app.ij.birdwatch;
 
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     FloatingActionButton fab;
+    int PICTURE_RESULT = 1, GALLERY_RESULT = 2;
+    int REQUEST_CAMERA = 1034, REQUEST_GALLERY = 1000, REQUEST_GALLERY_FROM_CAMERA = 234;
+
+    Uri imageUri;
+    Bitmap img;
+    ImageView image;
+    ContentValues values;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        image = findViewById(R.id.image);
         fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA);
+                } else {
+                    launchCamera();
+                }
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // They didn't cancel
+        if (resultCode == RESULT_OK) {
+            if (requestCode == PICTURE_RESULT) {
+                try {
+                    img = MediaStore.Images.Media.getBitmap(
+                            getContentResolver(), imageUri);
+
+                    image.setImageBitmap(img);
+                    makeToast("Got the image");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    makeToast("Error getting image");
+                }
+            } else {
+                //Later on for gallery
+            }
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CAMERA) {
+            if (checkSelfPermission(Manifest.permission.CAMERA)
+                    == PackageManager.PERMISSION_GRANTED) {
+                launchCamera();
+            }
+        }
+    }
+
+    void launchCamera() {
+        try {
+            values = new ContentValues();
+            imageUri = getContentResolver().insert(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+            startActivityForResult(intent, PICTURE_RESULT);
+        } catch (Exception e) {
+            Log.wtf("*Error launching Camera", e.toString());
+            makeToast("Unable to launch camera.");
+        }
     }
 
     @Override
@@ -38,4 +113,9 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
+    public void makeToast(String s) {
+        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+    }
+
 }
