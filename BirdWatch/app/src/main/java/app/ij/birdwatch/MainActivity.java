cb6b1c2d5df96849c27f2871be291711ff359644
Madhu2244 <madhuxsharma@gmail.com>
2020-11-07 12:08:40 -0600

    Added pytorch integration

--- BirdWatch/app/src/main/java/app/ij/birdwatch/MainActivity.java
+++ BirdWatch/app/src/main/java/app/ij/birdwatch/MainActivity.java
@@ -2,9 +2,11 @@ package app.ij.birdwatch;
 
 import android.Manifest;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -27,7 +29,16 @@ import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import org.pytorch.IValue;
+import org.pytorch.Module;
+import org.pytorch.Tensor;
+import org.pytorch.torchvision.TensorImageUtils;
+
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -116,6 +127,58 @@ public class MainActivity extends AppCompatActivity {
 
     public void makeToast(String s) {
         Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+    }
+
+    public static String assetFilePath(Context context, String assetName) throws IOException {
+        File file = new File(context.getFilesDir(), assetName);
+        if (file.exists() && file.length() > 0) {
+            return file.getAbsolutePath();
+        }
+
+        try (InputStream is = context.getAssets().open(assetName)) {
+            try (OutputStream os = new FileOutputStream(file)) {
+                byte[] buffer = new byte[4 * 1024];
+                int read;
+                while ((read = is.read(buffer)) != -1) {
+                    os.write(buffer, 0, read);
+                }
+                os.flush();
+            }
+            return file.getAbsolutePath();
+        }
+    }
+
+    public String modelLearning() throws IOException {
+        //replace below line to include the image from gallery.
+        Bitmap bitmap = BitmapFactory.decodeStream(getAssets().open("image.jpg"));
+        Module module = Module.load(assetFilePath(this, "bird-model.pth"));
+
+        // showing image on UI
+        ImageView imageView = findViewById(R.id.image);
+        imageView.setImageBitmap(bitmap);
+
+        // preparing input tensor
+        Tensor inputTensor = TensorImageUtils.bitmapToFloat32Tensor(bitmap,
+                TensorImageUtils.TORCHVISION_NORM_MEAN_RGB, TensorImageUtils.TORCHVISION_NORM_STD_RGB);
+
+        // running the model
+        Tensor outputTensor = module.forward(IValue.from(inputTensor)).toTensor();
+        // getting tensor content as java array of floats
+        float[] scores = outputTensor.getDataAsFloatArray();
+
+        // searching for the index with maximum score
+        float maxScore = -Float.MAX_VALUE;
+        int maxScoreIdx = -1;
+        for (int i = 0; i < scores.length; i++) {
+            if (scores[i] > maxScore) {
+                maxScore = scores[i];
+                maxScoreIdx = i;
+            }
+        }
+
+        //not sure if im supposed to use the arraylist or the linked list.
+        String className = BirdClasses.classes.get(maxScoreIdx);
+        return className;
     }
 
 }
